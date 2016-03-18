3bffc4e329c72368845df1d52ffbc23dcab86f5e
mike rosset <mike.rosset@gmail.com>
2016-03-18 12:28:58 -0700

    initial logic

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 test: all
+	./gf
 
 all:
 	go build .
--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/str1ngs/util/file"
+	"log"
+	"os"
+	"os/exec"
+	"time"
 )
 
 const (
@@ -10,10 +14,34 @@ const (
 )
 
 func main() {
-	if !file.Exists(FETCH_HEAD) {
+
+	if !file.Exists(".git") {
 		return
 	}
 
-	fmt.Prinln("OK")
-	//fmt.
+	if file.Exists(".git") && !file.Exists(FETCH_HEAD) {
+		git_fetch()
+		return
+	}
+
+	stat, err := os.Stat(FETCH_HEAD)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dur := time.Now().Sub(stat.ModTime())
+	if dur > time.Minute*5 {
+		fmt.Println("last fetch was ", dur)
+		git_fetch()
+	}
+}
+
+func git_fetch() {
+	fmt.Println("fetching...")
+	git := exec.Command("git", "fetch", "--all")
+	git.Stderr = os.Stderr
+	git.Stdout = os.Stdout
+	err := git.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
